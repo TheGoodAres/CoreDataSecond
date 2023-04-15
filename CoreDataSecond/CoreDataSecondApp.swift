bc9983523f2b449366da2f79ac0bcec88b0dd919
TheGoodAres <61661335+TheGoodAres@users.noreply.github.com>
2023-04-15 12:29:44 +0100

    Initial Commit

---
+++ CoreDataSecond/CoreDataSecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataSecondApp.swift
+//  CoreDataSecond
+//
+//  Created by Robert-Dumitru Oprea on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
